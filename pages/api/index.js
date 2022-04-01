40c1d97b5e1f3a137816835c61e63a2f11b47afc
syl-gh <gwanhyung787@syllabs.io>
2022-04-01 21:41:26 +0900

    feat: Add cors

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,8 +1,31 @@
+import Cors from "cors";
+// Initializing the cors middleware
+const cors = Cors({
+  methods: ["GET", "HEAD"],
+});
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result)
+      }
+
+      return resolve(result)
+    })
+  })
+}
+
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import geoip from "geoip-country";
 import requestIp from "request-ip";
 
 export default function handler(req, res) {
+  // Run the middleware
+  await runMiddleware(req, res, cors)
+
   const ip = requestIp.getClientIp(req);
 
   const geo = geoip.lookup(ip);
